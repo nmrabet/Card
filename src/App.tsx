93751f18623b4fc6707b839615f7161bf6a3325e
Nanouss <mrabet_nesrine@yahoo.fr>
2021-01-18 14:59:06 +0100

    Added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-200 h-screen">
+      <h1 className=" bg-blue-800 h-52 text-center text-6xl text-blue-100 pt-16 ">Hello world</h1>
     </div>
   );
 }
